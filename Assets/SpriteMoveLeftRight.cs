1f171eef71a945f8bd3b9016fbf07c47e94b9596
Dariuus2 <dariussturonas@gmail.com>
2020-02-02 02:07:42 -0800

    AddedMovementLeftRight AddedMovementScriptsUpDown Edited MonaLisa movement

---
+++ Assets/SpriteMoveLeftRight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMoveLeftRight : MonoBehaviour
+{
+    public Rigidbody2D image;
+    public float MoveLeftTill;
+    public float MoveRightTill;
+    ThrowItem itemcheck;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        image.GetComponent<Rigidbody2D>();
+        itemcheck = GameObject.FindGameObjectWithTag("Player").GetComponent<ThrowItem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (itemcheck.wark)
+        {
+            image.velocity = new Vector3(0, 0, 0);
+            image.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        if (image.gameObject.transform.position.x <= MoveLeftTill)
+        {
+            image.velocity = new Vector3(20, 0, 0);
+        }
+        if (image.gameObject.transform.position.x >= MoveRightTill)
+        {
+            image.velocity = new Vector3(-20, 0, 0);
+        }
+    }
+}
---
+++ Assets/SpriteMoveUpDown.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SpriteMoveUpDown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D image;
+    public float MoveUpTillCoordinates;
+    public float DownCoordinatesTill;
+
+    ThrowItem itemcheck;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        image.GetComponent<Rigidbody2D>();
+        itemcheck =GameObject.FindGameObjectWithTag("Player").GetComponent<ThrowItem>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (itemcheck.wark)
+        {
+            image.velocity = new Vector3(0, 0, 0);
+            image.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        if (image.gameObject.transform.position.y >= MoveUpTillCoordinates)
+        {
+            image.velocity = new Vector3(0, -10, 0);
+        }
+        if (image.gameObject.transform.position.y <= DownCoordinatesTill)
+        {
+            image.velocity = new Vector3(0, 20, 0);
+        }
+        
+    }
+}
